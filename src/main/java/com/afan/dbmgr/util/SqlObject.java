04dd9c95ebeab232a23a9f7b7ad220b08d4dd11d
afan966 <cfk_kfc@163.com>
2018-08-15 12:20:53 +0800

    SqlObject

---
+++ src/main/java/com/afan/dbmgr/util/SqlObject.java
@@ -0,0 +1,36 @@
+package com.afan.dbmgr.util;
+
+import java.util.List;
+
+public class SqlObject {
+	String sql;
+	Object[] paramValues;
+
+	public SqlObject(String sql, List<Object> params) {
+		this.sql = sql;
+		this.paramValues = params.toArray();
+	}
+
+	public int size() {
+		if (this.paramValues != null) {
+			return this.paramValues.length;
+		}
+		return 0;
+	}
+
+	public String getSql() {
+		return this.sql;
+	}
+
+	public void setSql(String sql) {
+		this.sql = sql;
+	}
+
+	public Object[] getParamValues() {
+		return this.paramValues;
+	}
+
+	public void setParamValues(Object[] paramValues) {
+		this.paramValues = paramValues;
+	}
+}
